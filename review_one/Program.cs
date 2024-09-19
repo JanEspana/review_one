f5b895ae1c4f3a463ea528621fb6d540445bdae8
JanEspana <jan.espana.7e7@itb.cat>
2024-09-19 13:37:09 +0200

    Code fixed

--- review_one/Program.cs
+++ review_one/Program.cs
@@ -3,26 +3,14 @@
     public static void Main()
     {
         const string AVG = "El promig de les notes es: ", INTRO = "Introdueix la nota ", APPROVED = "Aprovat", NOTAPPROVED = "Suspès";
-        int nota1 = 0, nota2 = 0, nota3 = 0;
+        int sum = 0;
         float avg;
         for (int i = 0; i < 3; i++)
         {
             Console.WriteLine(INTRO + (i + 1));
-            int nota = Convert.ToInt32(Console.ReadLine());
-            if (i == 0)
-            {
-                nota1 = nota;
-            }
-            else if (i == 1)
-            {
-                nota2 = nota;
-            }
-            else if (i == 2)
-            {
-                nota3 = nota;
-            }
+            sum += Convert.ToInt32(Console.ReadLine());
         }
-        avg = (nota1 + nota2 + nota3) / 3;
+        avg = sum / 3;
 
         Console.WriteLine(AVG + avg);
 
